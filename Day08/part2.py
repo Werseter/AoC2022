3f47f09fea395765a9a5531be6619cf2342bd4e3
Werseter <werseter@interia.eu>
2022-12-14 05:07:26 +0100

    Simplify takeuntil expr in Day08 Part2

--- Day08/part2.py
+++ Day08/part2.py
@@ -4,6 +4,6 @@ from operator import mul
 
 with open('input.txt') as fp:
     data = [[int(x) for x in line.strip()] for line in fp.readlines()]
-print(max(map(max, ([reduce(mul, [len((z := list(takewhile(lambda z: z < x, y))) + list(y)[len(z):len(z) + 1]) for y in
+print(max(map(max, ([reduce(mul, [len(y[:len(list(takewhile(lambda z: z < x, y))) + 1]) for y in
                                   (row[:i][::-1], row[i + 1:], col[:j][::-1], col[j + 1:])]) for i, (x, col) in
                      enumerate(zip(row, zip(*data)))] for j, row in enumerate(data)))))
